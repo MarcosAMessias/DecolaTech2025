0d02691d057350f1029e3722521c6de34697b8e4
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2025-03-04 18:02:24 -0300

    Create ProdutoTest.java

---
+++ ProdutoTest.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ProdutoTest {
+
+    @Test
+    public void testFalso() {
+
+        assertTrue(True);
+    }
+}
